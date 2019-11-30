be6a4b568ea97259bfaf86386e68b031fd9c50fd
Divya-UNICAM <57792594+Divya-UNICAM@users.noreply.github.com>
2019-12-01 00:28:30 +0100

    Added Methods

--- backend/pick-a-park/src/main/java/it/unicam/cs/automoto/pick_a_park/PlatformServices.java
+++ backend/pick-a-park/src/main/java/it/unicam/cs/automoto/pick_a_park/PlatformServices.java
@@ -5,15 +5,130 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("api")
 public class PlatformServices {
 	
-	@Path("hello")
+	@Path("Management")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String hello() {
-		return "{ 'hello':'world' }";
+	public String Management() 
+	{
+		return "";
+		//TODO		
 	}
 
+	@Path("Pay")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Pay() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Login")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Login() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Request")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Request() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Add")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Add() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Position")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Position() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Notify")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Notify() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Recieve")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Recieve() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Register") //Sign-up
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Register() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Display") //Display the route
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Display() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Send") //Send Feedback
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Send() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Modify") //change parking place
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Modify() 
+	{
+		return "";
+		//TODO		
+	}
+	
+	@Path("Subscribe") 
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Subscribe() 
+	{
+		return "";
+		//TODO		
+	}
+	
 }
+
