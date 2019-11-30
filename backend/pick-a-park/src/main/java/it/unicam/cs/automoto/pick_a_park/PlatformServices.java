4aeb3a54280b24d5c1cf0d544aa45f4572e4e554
Vincenzo Nucci <vincenzo.nucci@studenti.unicam.it>
2019-11-30 23:12:12 +0100

    Services class created

---
+++ backend/pick-a-park/src/main/java/it/unicam/cs/automoto/pick_a_park/PlatformServices.java
@@ -0,0 +1,19 @@
+package it.unicam.cs.automoto.pick_a_park;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("api")
+public class PlatformServices {
+	
+	@Path("hello")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String hello() {
+		return "{ 'hello':'world' }";
+	}
+
+}
