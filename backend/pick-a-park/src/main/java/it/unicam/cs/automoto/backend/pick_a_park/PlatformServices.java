4ff29d47b1f604f1d0019d6fd175d691b117e1a9
Vincenzo Nucci <vincenzo.nucci@studenti.unicam.it>
2019-12-01 13:14:19 +0100

    bad merge restore

--- backend/pick-a-park/src/main/java/it/unicam/cs/automoto/backend/pick_a_park/PlatformServices.java
+++ backend/pick-a-park/src/main/java/it/unicam/cs/automoto/backend/pick_a_park/PlatformServices.java
@@ -1,25 +1,4 @@
-<<<<<<< HEAD:backend/pick-a-park/src/main/java/it/unicam/cs/automoto/backend/pick_a_park/PlatformServices.java
 package it.unicam.cs.automoto.backend.pick_a_park;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-@Path("api")
-public class PlatformServices {
-	
-	@Path("hello")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String hello() {
-		return "{ 'hello':'world' }";
-	}
-
-}
-=======
-package it.unicam.cs.automoto.pick_a_park;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -152,5 +131,3 @@ public class PlatformServices {
 	}
 	
 }
-
->>>>>>> be6a4b568ea97259bfaf86386e68b031fd9c50fd:backend/pick-a-park/src/main/java/it/unicam/cs/automoto/pick_a_park/PlatformServices.java
