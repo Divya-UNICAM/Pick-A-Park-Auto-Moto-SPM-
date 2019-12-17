1c4612f7797f3c2e347e8032dd6c21e95dfa4865
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2019-12-17 13:15:55 +0100

    added encryption

--- utils/salt.js
+++ utils/salt.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
-var salt = null;
+const salt = null;
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
 async function generateSalt() {
     salt = await bcrypt.genSalt(10);
@@ -10,4 +14,21 @@ function getSalt() {
     return salt;
 }
 
-module.exports = { generateSalt, getSalt };
+//The resulting object must not be modified but used as it is
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+module.exports = { generateSalt, getSalt, encrypt, decrypt };
