18d69560985c42d7b5558a15a654d7bc0b6e54b6
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-01-10 14:56:29 +0100

    updated with internal local db

--- db/index.js
+++ db/index.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const { MongoMemoryServer } = require('mongodb-memory-server');
 dotenv.config();
 
 var connection = null;
+var mongod = new MongoMemoryServer();
 
-function connect(type) {
+async function connect(type) {
     if(connection === null)
-        if(type === 'local')
-            connection = mongoose.connect(
-                process.env.LOCAL_MONGO,
-                { 
-                    useNewUrlParser : true,
-                    useUnifiedTopology : true
-                });
-        else
+        if(process.env.NODE_ENV !== 'production') {
+            const uri = await mongod.getConnectionString();
+            const port = await mongod.getPort();
+            const dbPath = await mongod.getDbPath();
+            const dbName = await mongod.getDbName();
+            connection = mongoose.connect(uri, { useNewUrlParser:true, useUnifiedTopology:true });
+        }
+        else //If it's a production environment
             connection = mongoose.connect(
                 process.env.MONGO_URI,
                 { 
@@ -24,9 +25,12 @@ function connect(type) {
     return connection;
 };
 
-function close() {
-    if(connection !== null)
+async function close() {
+    if(connection !== null) {
+        if(mongod)
+            await mongod.stop();
         return mongoose.disconnect();
+    }
 }
 
 module.exports = { connect, close, connection };
