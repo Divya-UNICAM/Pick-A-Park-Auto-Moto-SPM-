514cf4f0ae88e9b44c5cf485e34e0a7fd888d5fb
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-01-16 21:36:01 +0100

    added validation for tests

--- validation.js
+++ validation.js
@@ -58,14 +58,76 @@ const requestValidation = body => {
 //Sensor validation
 const sensorValidation = body => {
     const schema = Joi.object({
-        location: Joi.object({
-            lat: Joi.number(),
-            lng: Joi.number()
-        })
-        .required(),
-        date: Joi.date()
-            .required(),
+        parkingPlace: Joi.string().hex(),
+        position: Joi.number().required(),
+        ipAddress: Joi.string().ip().required(),
+        update: Joi.string(),
+        date: Joi.date(),
         detected: Joi.number(),
+        status: Joi.string()
+    });
+    return schema.validate(body);
+};
+
+//Parking place validation
+const parkingPlaceValidation = body => {
+    const schema = Joi.object({
+        municipality: Joi.string()
+            .hex(),
+        location: Joi.object({
+            lat: Joi.number()
+                .required(),
+            lng: Joi.number()
+                .required(),
+            address: Joi.string()
+                .required()
+        }),
+        date: Joi.date(),
+        status: Joi.string()
+    });
+    return schema.validate(body);
+};
+
+//Municipality Validation
+const municipalityValidation = body => {
+    const schema = Joi.object({
+        name: Joi.string()
+            .required(),
+        province: Joi.string()
+            .required(),
+        region: Joi.string()
+            .required(),
+        postcode: Joi.string()
+            .required(),
+        location: Joi.object({
+            lat: Joi.number().required(),
+            lng: Joi.number().required()
+        }),
+        policeOfficers: Joi.array(),
+        date: Joi.date()
+    });
+    return schema.validate(body);
+};
+
+//Police officers Validation
+const policeOfficerValidation = body => {
+    const schema = Joi.object({
+        name: Joi.string(),
+        email: Joi.string().email(),
+        password: Joi.string(),
+        badge: Joi.string(),
+        status: Joi.string()
+    });
+    return schema.validate(body);
+};
+
+//Job Validation
+const jobValidation = body => {
+    const schema = Joi.object({
+        municipality: Joi.string(),
+        policeOfficer: Joi.string(),
+        parkingPlace: Joi.string(),
+        date: Joi.date(),
         status: Joi.string()
     });
     return schema.validate(body);
@@ -75,3 +137,7 @@ module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
 module.exports.requestValidation = requestValidation;
 module.exports.sensorValidation = sensorValidation;
+module.exports.parkingPlaceValidation = parkingPlaceValidation;
+module.exports.municipalityValidation = municipalityValidation;
+module.exports.policeOfficerValidation = policeOfficerValidation;
+module.exports.jobValidation = jobValidation;
