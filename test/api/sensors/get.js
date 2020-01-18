f5b2d1b331e8f06c856a3103766abe86023f37ba
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-01-18 14:59:50 +0100

    added tests for sensors

--- test/api/sensors/get.js
+++ test/api/sensors/get.js
@@ -6,36 +6,59 @@ const should = require('chai').should;
 const request = require('supertest');
 const db = require('../../../db/index');
 const Sensor = require('../../../db/models/Sensor');
+const Municipality = require('../../../db/models/Municipality');
+const ParkingPlace = require('../../../db/models/ParkingPlace');
 const faker = require('faker');
 const server = require('../../../server');
 
 before((done) => {
     db.connect()
-        .then(() => { console.log('Connected to test DB!'); done(); }) 
-        .catch((err) => { console.log(err); done(err)});
+        .then(() => done()) 
+        .catch((err) => done(err));
+})
+after((done) => {
+    db.close()
+    .then(() => done())
+    .catch((err) => done(err));
 })
 
 describe('GET /api/sensor', () => {
 
     beforeEach((done) => {
-        for(let i=0; i < 10; i++)
-            Sensor.create({
+        const municpt1 = new Municipality({
+            name: 'Camerino',
+            province: 'MC',
+            region: 'Marche',
+            postcode: 62032,
+            location: {
+                lat: faker.address.latitude(),
+                lng: faker.address.longitude()
+            }
+        }).save();
+        municpt1.then((doc) => {
+            new ParkingPlace({
+                municipality: doc.id,
                 location: {
                     lat: faker.address.latitude(),
-                    lng: faker.address.longitude()
-                },
-                date: faker.date.recent(2),
-                detected: 0,
-                status: "FREE"
-            });
-        done();
+                    lng: faker.address.longitude(),
+                    address: 'aaa'
+                }
+            }).save().then((doc) => {
+                new Sensor({
+                    parkingplace: doc.id,
+                    position: 0,
+                    ipAddress: faker.internet.ip()
+                }).save().then((doc) => {
+                    done();
+                }).catch((err) => done(err));
+            }).catch((err) => done(err));
+        }).catch((err) => done(err));
     })
     it('should return all sensors', (done) => {
-        request(server).get('/')
+        request(server).get('/api/dashboard/sensors/62032/aaa')
+        .set("Cookie",'auth_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')
         .then((res) => {
-            const body = res.body;
             expect(res.status).to.be.equal(200);
-            console.log(res.body);
             done()
         })
         .catch((err) => done(err))
