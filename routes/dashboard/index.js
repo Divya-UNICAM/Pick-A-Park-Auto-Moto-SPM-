bf1569245bce35f3a43207a70ea4fe074ed8a418
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-01-21 10:38:04 +0100

    updated index.js for dashboard dir

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -1,7 +1,11 @@
 const municipalityRoute = require('./municipality');
 const parkingPlacesRoute = require('./parkingplace');
 const sensorRoute = require('./sensor');
+const officerRoute = require('./officer');
+const taskRoute = require('./task');
 
 module.exports.municipalityRoute = municipalityRoute;
 module.exports.parkingPlacesRoute = parkingPlacesRoute;
 module.exports.sensorRoute = sensorRoute;
+module.exports.taskRoute = taskRoute;
+module.exports.officerRoute = officerRoute;
