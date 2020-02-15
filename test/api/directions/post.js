48bd9c48e18648f741034ea5ddfc2140ac5fc457
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-02-15 15:36:38 +0000

    request, payment and route to dest tests

--- test/api/directions/post.js
+++ test/api/directions/post.js
@@ -1,48 +1,49 @@
 process.env.NODE_ENV = 'test';
 
 const expect = require('chai').expect;
-const request = require('supertest');
+const request = require('request-promise');
 const faker = require('faker');
 const Request = require('../../../db/models/Request');
 const SensorModel = require('../../../db/models/Sensor');
 const ParkingPlace = require('../../../db/models/ParkingPlace');
 const Municipality = require('../../../db/models/Municipality');
+const dbUtils = require('../../../utils/dbUtils');
 const db = require('../../../db/index');
 const server = require('../../../server');
-const Sensor = require('../../../routes/sensors');
 
-before((done) => {
+before(done => {
     db.connect()
-    .then(() => done())
-    .catch((err) => done(err));
+        .then(() => done())
+        .catch((err) => done(err));
 })
 
-describe('GET /directions', () => {
+describe('GET /route', () => {
     var sentRequestId;
-    beforeEach((done) => {
-        //Mock sensors and parking places
-        const sentRequest = new Request({
-            startingLocation: {
-                lat: faker.address.latitude(),
-                lng: faker.address.longitude()
-            },
-            targetLocation: {
-                lat: faker.address.latitude(),
-                lng: faker.address.longitude()
-            },
-            duration: 1,
-            licensePlate: "aaaaaaa",
-            date: faker.date.recent()
-        }).save()
-        .then((doc) => {
-            sentRequestId = doc.id;
+    before((done) => {
+        dbUtils.addARequestTest(
+            "Camerino",
+            "Camerano",
+            "12-12-19",
+            "1111111",
+            10
+        ).then((req) => {
+            sentRequestId = req._id;
             done();
-        })
+        }).catch((err) => done(err));
     });
-    it('should create a route', (done) => {
-        request(server).get('/api/pay/test/success?id='+sentRequestId) //force payment success
+
+    it('OK, getting route to destination works', (done) => {
+        var options = {
+            method: "GET",
+            uri: "http://localhost:3001/api/pay/success",
+            resolveWithFullResponse: true,
+            headers: {
+                'Cookie': 'reqId='+sentRequestId
+            }
+        }
+        request(options) //force payment success
             .then((res) => {
-                expect(res.status).to.be.equal(302);
+                expect(res.statusCode).to.be.equal(200);
                 done();
             })
             .catch((err) => done(err));
