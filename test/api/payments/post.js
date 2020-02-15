120863d4e7edc616dccd29e636b8ccaf584f6e08
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-02-15 15:36:00 +0000

    tests for payment

--- test/api/payments/post.js
+++ test/api/payments/post.js
@@ -1,22 +1,37 @@
 process.env.NODE_ENV = 'test';
 
 const expect = require('chai').expect;
-const request = require('supertest');
+const request = require('request-promise');
 const faker = require('faker');
 const Request = require('../../../db/models/Request');
 const db = require('../../../db/index');
+const dbUtils = require('../../../utils/dbUtils');
 const server = require('../../../server');
+const tough = require('tough-cookie').Cookie;
 
 describe('POST /api/pay', () => {
+    var cookie;
     before((done) => {
-        db.connect()
-        .then(() => done())
-        .catch((err) => done(err));
-    })
-    it('OK, sending a payment request works', (done) => {
-        request(server).get('/api/pay')
+        dbUtils.addARequestTest("Camerino","Camerano",'12-12-19','1111111',10,true)
             .then((res) => {
-                expect(res).to.have.property('status',200);
+                cookie = res.headers['set-cookie'][0];
+                done();
+            })
+            .catch((err) => done(err));
+        
+    })
+    it('OK, paying for a request works', (done) => {
+        var options = {
+            method: "post",
+            uri: 'http://localhost:3001/api/pay',
+            headers: {
+                'Cookie': cookie
+            },
+            resolveWithFullResponse: true
+        }
+        request(options)
+            .then((res) => {
+                expect(res).to.have.property('statusCode',200);
                 done();
             })
             .catch((err) => done(err));
