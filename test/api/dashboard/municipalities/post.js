dd484e0f75d52e5344a8376898508de86bee27f0
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-02-22 14:45:36 +0000

    added better db management

--- test/api/dashboard/municipalities/post.js
+++ test/api/dashboard/municipalities/post.js
@@ -5,6 +5,25 @@ const request = require('request-promise');
 const faker = require('faker');
 const dbUtils = require('../../../../utils/dbUtils');
 const tough = require('tough-cookie').Cookie;
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongoServer;
+const opts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+before(async () => {
+    mongoServer = new MongoMemoryServer();
+    const mongoUri = await mongoServer.getUri();
+    await mongoose.connect(mongoUri, opts)
+});
+
+after(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+})
 
 describe('POST /api/dashboard/municipalities', () => {
 
