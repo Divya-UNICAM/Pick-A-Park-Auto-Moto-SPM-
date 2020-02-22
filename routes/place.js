80c2ab4058dfe9610b42e4f9d8d2b573f8a9c682
VincenzoNucci <vincenzo.nucci@studenti.unicam.it>
2020-02-22 10:26:39 +0000

    added tracking service

--- routes/place.js
+++ routes/place.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const Request = require('../db/models/Request');
 const Municipality = require('../db/models/Municipality');
 const ParkingPlace = require('../db/models/ParkingPlace');
+const dbUtils = require('../utils/dbUtils');
+const querystring = require('querystring');
 
 const deg2rad = (deg) => {
     return deg * (Math.PI/180);
@@ -85,6 +88,48 @@ router.get('/distance', async (req,res) => {
     distance = rad2deg(distance);
     distance = distance * 60 * 1.1515 * 1.609344;
     res.send(distance);
+});
+
+//receive current position from drivers
+//this service is called every defined time step by the driver route page
+router.post('/tracking', async (req,res) => {
+    const reqId = req.cookies['reqId'];
+    const currentPosition = {
+        lat: req.body.lat,
+        lng: req.body.lng
+    }
+    const requestedRequest = await Request.findById(reqId);
+    if(!requestedRequest)
+        res.status(404).send('Request not found');
+    const assignedPlace = requestedRequest.assignedplace;
+    let places = await dbUtils.getDistanceBetween(currentPosition);
+    const elementMinCurrentDistance = places.reduce((prev,curr) => {
+        return prev.distance < curr.distance ? prev : curr;
+    });
+    //console.log(assignedPlace)
+    //console.log(elementMinCurrentDistance)
+    //if there is a new parking place, either freed or newly added, with a better position
+    //with respect to the destination, choose it and re-route
+    if(elementMinCurrentDistance.place !== assignedPlace.place && elementMinCurrentDistance.distance < assignedPlace.distance) {
+        await requestedRequest.updateOne({
+            assignedplace: elementMinCurrentDistance
+        });
+
+        coordinates = {
+            startingLat: requestedRequest.startingLocation.lat,
+            startingLng: requestedRequest.startingLocation.lng,
+            targetLat: elementMinCurrentDistance.place.lat,
+            targetLng: elementMinCurrentDistance.place.lng
+        };
+        //Get the nearest parking place for the specified destination
+
+        //Return map with directions
+        const query = querystring.stringify(coordinates);
+        return res.cookie("reqId",reqId).redirect('http://localhost:3001/route?'+query);
+        //Redirect to a successful checkout page and send invoice
+    } else
+        //send Not Modified if the parking place doesn't need to be changed
+        return res.sendStatus(304);
 });
 
 module.exports = router;
